22013885e166813dfac031e75f217f05602dc3a1
Theo Guyard <theo.guyard@insa-rennes.fr>
2023-10-12 14:38:51 +0200

    Put additional stuff into base bounding solver class

--- src/el0ps/solver/bounding/base.py
+++ src/el0ps/solver/bounding/base.py
@@ -4,8 +4,14 @@ from typing import Union
 from numba import njit
 from numpy.typing import NDArray
 from el0ps.datafit import BaseDatafit
+from el0ps.penalty import BasePenalty
 from el0ps.problem import Problem
 from el0ps.solver.node import BnbNode
+
+
+@njit
+def softhresh(x: float, eta: float) -> float:
+    return np.sign(x) * np.maximum(np.abs(x) - eta, 0.0)
 
 
 class BnbBoundingSolver:
@@ -67,6 +73,66 @@ class BnbBoundingSolver:
             lower bounding operation.
         """
         ...
+
+    @staticmethod
+    @njit
+    def compute_pv(
+        datafit: BaseDatafit,
+        penalty: BasePenalty,
+        lmbd: float,
+        tau: float,
+        mu: float,
+        x: NDArray[np.float64],
+        w: NDArray[np.float64],
+        S1: NDArray[np.bool_],
+        Sb: NDArray[np.bool_],
+    ) -> float:
+        pv = datafit.value(w)
+        for i in np.flatnonzero(S1 | (Sb & (x != 0.0))):
+            if Sb[i] and np.abs(x[i]) <= mu:
+                pv += tau * np.abs(x[i])
+            else:
+                pv += penalty.value(x[i]) + lmbd
+        return pv
+
+    @staticmethod
+    @njit
+    def compute_dv(
+        datafit: BaseDatafit,
+        penalty: BasePenalty,
+        A: NDArray[np.float64],
+        lmbd: float,
+        x: NDArray[np.float64],
+        u: NDArray[np.float64],
+        v: NDArray[np.float64],
+        p: NDArray[np.float64],
+        S1: NDArray[np.bool_],
+        Sb: NDArray[np.bool_],
+    ) -> float:
+        nz = np.flatnonzero(S1 | Sb)
+        sf = np.empty(v.shape)
+        for i in nz:
+            v[i] = np.dot(A[:, i], u)
+            p[i] = penalty.conjugate(v[i]) - lmbd
+            sf[i] = penalty.conjugate_scaling_factor(v[i])
+        g_sf = 1.0 if nz.size == 0 else np.min(sf[nz])
+        u_sf = g_sf * u
+        v_sf = g_sf * v
+        dv = -datafit.conjugate(-u_sf)
+        for i in nz:
+            p_sf = penalty.conjugate(v_sf[i]) - lmbd
+            dv -= np.maximum(p_sf, 0.0) if Sb[i] else p_sf
+        return dv
+
+    @staticmethod
+    @njit
+    def abs_gap(pv: float, dv: float) -> float:
+        return np.abs(pv - dv)
+
+    @staticmethod
+    @njit
+    def rel_gap(pv: float, dv: float) -> float:
+        return np.abs(pv - dv) / (np.abs(pv) + 1e-16)
 
     @staticmethod
     @njit
