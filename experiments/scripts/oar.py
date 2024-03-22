946d8809893266045fd1f1e8f4f273bd4cc19668
Theo Guyard <theo.guyard@insa-rennes.fr>
2024-03-22 09:32:35 +0100

    Fix exp name

--- experiments/scripts/oar.py
+++ experiments/scripts/oar.py
@@ -23,10 +23,7 @@ run_file = "run.sh"
 run_path = script_dir.joinpath(run_file)
 
 
-experiments = [
-    get_exp(exp_name)
-    for exp_name in ["perfprofile", "regpath", "perfprofile_suppl"]
-]
+experiments = [get_exp(exp_name) for exp_name in ["perfprofile", "regpath"]]
 
 
 def oar_send():
