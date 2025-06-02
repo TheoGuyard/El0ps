Fix KL definition (#92)
Co-authored-by: Theo Guyard <theo.guyard@insa-rennes.fr>

src/el0ps/datafit/kullbackleibler.py
@@ -16,7 +16,9 @@ class KullbackLeibler(CompilableClass, BaseDatafit):
         f(\mathbf{w}) = \sum_{i=1}^m y_i \log(\tfrac{y_i}{w_i + e}) + (w_i + e) - y_i
 
 
-    for some :math:`\mathbf{y} \in \mathbb{R}^m` and :math:`e > 0`.
+    for some :math:`\mathbf{y} \in \mathbb{R}_+^m` and :math:`e \geq 0`. The
+    function returns :math:`+\infty` whenever :math:`w_i + e \leq 0` for some
+    :math:`i \in \{1,\dots,m\}`.
 
     Parameters
     ----------
@@ -29,7 +31,7 @@ class KullbackLeibler(CompilableClass, BaseDatafit):
     def __init__(self, y: NDArray, e: float = 1e-8) -> None:
         self.y = y
         self.e = e
-        self.L = np.max(y) / e**2
+        self.L = np.inf
         self.log_yy = np.log(y * y)
 
     def __str__(self) -> str:
@@ -48,18 +50,21 @@ class KullbackLeibler(CompilableClass, BaseDatafit):
         return dict(y=self.y, e=self.e)
 
     def value(self, w: NDArray) -> float:
-        z = np.maximum(w, 0.0) + self.e
+        z = w + self.e
+        if np.any(z <= 0.0):
+            return np.inf
         return np.sum(self.y * np.log(self.y / z) + z - self.y)
 
     def conjugate(self, w: NDArray) -> float:
-        u = w
-        v = 1.0 - u
-        if np.any(v <= 0.0):
-            return np.inf
-        return np.sum(self.y * (self.log_yy - np.log(v)) - self.e * u)
+        v = np.maximum(self.y / (1. - w) - self.e, 0.0)
+        return np.sum(
+            self.y * np.log(self.y / (v + self.e)) + v + self.e - self.y
+        )
 
     def gradient(self, w: NDArray) -> NDArray:
-        z = np.maximum(w, 0.0) + self.e
+        z = w + self.e
+        if np.any(z <= 0.0):
+            return np.inf * np.ones_like(w)
         return 1.0 - self.y / z
 
     def gradient_lipschitz_constant(self) -> float:

tests/test_datafit.py
@@ -12,12 +12,10 @@ from el0ps.datafit import (
 
 m = 100
 y = np.random.randn(m)
-x = np.random.randn(m)
-u = np.random.randn(m)
 datafits = [
     pytest.param(datafit, id=f"{datafit.__class__.__name__}")
     for datafit in [
-        KullbackLeibler(np.abs(y)),
+        KullbackLeibler(np.ceil(10 * np.abs(y))),
         Leastsquares(y),
         Logcosh(y),
         Logistic(2.0 * (y > 0.0) - 1.0),
@@ -29,6 +27,11 @@ datafits = [
 @pytest.mark.parametrize("datafit", datafits)
 def test_datafit(datafit: BaseDatafit):
     assert isinstance(datafit.__str__(), str)
+    x = np.random.randn(m)
+    u = np.random.randn(m)
+    if isinstance(datafit, KullbackLeibler):
+        x = np.abs(x) + 2. * datafit.e
+        u = np.abs(u) + 2. * datafit.e
     assert datafit.value(x) + datafit.conjugate(u) >= np.dot(x, u)
     assert datafit.gradient_lipschitz_constant() >= 0.0
     assert datafit.gradient(x).shape == x.shape